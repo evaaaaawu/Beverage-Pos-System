1a20345efd703dc76b94cc73a730b6ed2066bbd5
evaaaaawu <yiju.wu1@gmail.com>
2023-06-27 11:10:42 +0800

    feat: add order

--- app.js
+++ app.js
@@ -1,3 +1,63 @@
+//app.js
+
+const beveragePos = new BeveragePos()
+
+const addDrinkButton = document.querySelector('[data-beverage-pos="add-drink"]')
+addDrinkButton.addEventListener('click', function () {
+  // 1. get checked value of options
+  const drinkName = beveragePos.getCheckedValue('drink')
+  const ice = beveragePos.getCheckedValue('ice')
+  const sugar = beveragePos.getCheckedValue('sugar')
+
+  // 2. show alert if user did not check any drink option
+  if (!drinkName) {
+    alert('Please choose at least one item.')
+    return
+  }
+
+  // 3. Use Drink Constructor to create drink instance
+  const drink = new Drink(drinkName, sugar, ice)
+
+  // 4. add order UI
+  beveragePos.addDrink(drink)
+})
+
+// Constructor function for Beverage Pos System
+function BeveragePos() { }
+BeveragePos.prototype.getCheckedValue = function (inputName) {
+  let selectedOption = ''
+  document.querySelectorAll(`[name=${inputName}]`).forEach(function (item) {
+    if (item.checked) {
+      selectedOption = item.value
+    }
+  })
+  return selectedOption
+}
+
+const orderLists = document.querySelector('[data-order-lists]')
+BeveragePos.prototype.addDrink = function (drink) {
+  let orderListsCard = `
+    <div class="card mb-3">
+      <div class="card-body pt-3 pr-3">
+        <!-- delete drink icon -->
+        <div class="text-right">
+          <span data-beverage-pos="delete-drink">×</span>
+        </div>
+        <h6 class="card-title mb-1">${drink.name}</h6>
+        <div class="card-text">${drink.ice}</div>
+        <div class="card-text">${drink.sugar}</div>
+      </div>
+
+      <div class="card-footer text-right py-2">
+        <div class="card-text text-muted">
+          $ <span data-drink-price></span>${drink.price()}</span>
+        </div>
+      </div>
+    </div>
+  `
+  orderLists.insertAdjacentHTML('afterbegin', orderListsCard)
+}
+
 function Drink(name, sugar, ice) {
   this.name = name
   this.sugar = sugar
@@ -21,15 +81,3 @@ Drink.prototype.price = function () {
       alert('No this drink')
   }
 }
-
-let blackTea = new Drink('Black Tea', 'Half Sugar', 'No Ice')
-console.log(blackTea)
-console.log(blackTea.price())
-
-let lemonGreenTea = new Drink('Lemon Green Tea', 'No Sugar', 'Less Ice')
-console.log(lemonGreenTea)
-console.log(lemonGreenTea.price())
-
-let matchaLatte = new Drink('Matcha Latte', 'Less Sugar', 'Regular Ice')
-console.log(matchaLatte)
-console.log(matchaLatte.price())
